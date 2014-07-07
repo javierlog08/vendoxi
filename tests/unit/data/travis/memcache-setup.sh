4f878db8fc3adc3f1c1f7b270e3f94d55976f35a
javierlog08 <javierlog08@gmail.com>
2014-07-07 09:44:10 -0500

    Agregue todo el framework de yii  por error. para esta App se usara solamente el esqueleto de una app

--- tests/unit/data/travis/memcache-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if (php --version | grep -i HHVM > /dev/null); then
-  echo "skipping memcache on HHVM"
-else
-  mkdir -p ~/.phpenv/versions/$(phpenv version-name)/etc
-  echo "extension=memcache.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-  echo "extension=memcached.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-fi
